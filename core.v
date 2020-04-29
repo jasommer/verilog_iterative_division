285d96266249da7bae3d491fa19551506f0d22eb
Jan Sommer <61277056+jasommer@users.noreply.github.com>
2020-04-29 21:29:18 +0200

    Update core.v

--- core.v
+++ core.v
@@ -1,15 +1,15 @@
-//-----------------------------------------------------------------------------
+//  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+//  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+//  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+//  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+//  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+//  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
 //
-// Title        : core
-// Design       : divider
-// Author       : Jan Sommer
-// Company      : KARL STORZ Endoskope
-//
-//-----------------------------------------------------------------------------
-//
-// Generated    : Friday October 10th 2018
-//
-//-----------------------------------------------------------------------------
+// Copyright (C) 2020 Jan Sommer
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
 //
 // Description  : This module implements a 32-bit division method that significantly helps to save hardware resources.
 //                Compared to the / operator, this method uses five times less hardware resources. 
